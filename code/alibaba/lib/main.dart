bd06ceb3823d92a0bcf75cb57aca31d53e13c15b
mohsenkamini <mohsenkamini@gmail.com>
2023-05-30 19:43:35 +0330

    done with the 8 steps

--- code/alibaba/lib/main.dart
+++ code/alibaba/lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page=GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -104,6 +104,38 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         );
       }
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    if (appState.favorites.isEmpty){
+      return Center(
+        child: Text('No favorites yer.'),
+      );
+    }
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have ${appState.favorites.length} favorites ' ),
+        ),
+        
+        for (var i in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(i.asLowerCase),
+          ),
+          
+        
+        
+        
+
+      ],
     );
   }
 }
