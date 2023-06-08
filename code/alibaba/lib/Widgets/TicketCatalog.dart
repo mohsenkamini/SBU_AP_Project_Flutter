80e34ebc620c972726e00ddf5c449ed78adaa2a4
mohsenkamini <mohsenkamini@gmail.com>
2023-06-08 22:36:26 +0330

    added the parameters for ticket catalog

--- code/alibaba/lib/Widgets/TicketCatalog.dart
+++ code/alibaba/lib/Widgets/TicketCatalog.dart
@@ -6,10 +6,28 @@ import 'package:provider/provider.dart';
 class TicketCatalog extends StatelessWidget {
   const TicketCatalog({
     super.key,
-    required this.pair,
+    required this.airlineName,
+    required this.airlineUrl,
+    required this.tag1,
+    required this.tag2,
+    required this.tag3,
+    required this.takeOffTime,
+    required this.landingTime,
+    required this.price,
+    required this.booked,
+    required this.capacity,
   });
 
-  final WordPair pair;
+  final String airlineName;
+  final String airlineUrl;
+  final String tag1;
+  final String tag2;
+  final String tag3;
+  final String takeOffTime;
+  final String landingTime;
+  final String price;
+  final int booked; // number of people bought this ticket
+  final int capacity;
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +40,7 @@ class TicketCatalog extends StatelessWidget {
     );
     return Card(
       elevation: 20,
-      color: theme.colorScheme.primary,
+      color: ,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Padding(
