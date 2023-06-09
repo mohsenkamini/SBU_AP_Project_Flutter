9cdc93e808667a43fb43bcf6c9345c29ba70120e
Aida <Latifi27aidaa@gmail.com>
2023-06-09 11:51:10 +0330

    destinatioand origin dropdown menu added

--- code/alibaba/lib/FlightsPage.dart
+++ code/alibaba/lib/FlightsPage.dart
@@ -10,6 +10,7 @@ import 'package:gap/gap.dart';
 import 'package:alibaba/screens/utils/Styles.dart';
 import 'package:alibaba/DomesticFlights.dart';
 import 'package:alibaba/Details/OriginDomestic.dart';
+import 'Details/DestinationDomestic.dart';
 import 'InternationalFlights.dart';
 
 class FlightsPage extends StatefulWidget {
@@ -33,6 +34,7 @@ class _FlightsPageState extends State<FlightsPage>
     with TickerProviderStateMixin {
   @override
   String? valueOriginDomestic;
+  String? valueDestinationDomestic;
   Widget build(BuildContext context) {
     //pagelayout
     final size = AppLayout.getSize(context);
@@ -113,11 +115,48 @@ class _FlightsPageState extends State<FlightsPage>
         child: Text(
           item,
         ));
-    return DropdownButton<String>(
-      isExpanded: true,
-      items: items.map(buildMenuItem).toList(),
-      value: valueOriginDomestic,
-      onChanged: ((value) => setState(() => valueOriginDomestic = value)),
+    return Container(
+      width: 3,
+      height: 1,
+      margin: EdgeInsets.only(top: 2, bottom: 350, right: 20, left: 10),
+      padding: EdgeInsets.only(top: 2, bottom: 0, right: 20, left: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.black87, width: 4)),
+      child: DropdownButton<String>(
+        isExpanded: true,
+        hint: Text("مبدا"),
+        items: items.map(buildMenuItem).toList(),
+        value: valueOriginDomestic,
+        onChanged: ((value) => setState(() => valueOriginDomestic = value)),
+      ),
+    );
+  }
+
+  DomesticDestination() {
+    var items = DestinationDomestic.items;
+
+    DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+        value: item,
+        child: Text(
+          item,
+        ));
+    return Container(
+      width: 3,
+      height: 1,
+      margin: EdgeInsets.only(top: 2, bottom: 350, right: 20, left: 10),
+      padding: EdgeInsets.only(top: 2, bottom: 0, right: 20, left: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.black87, width: 4)),
+      child: DropdownButton<String>(
+        isExpanded: true,
+        hint: Text("مقصد"),
+        items: items.map(buildMenuItem).toList(),
+        value: valueDestinationDomestic,
+        onChanged: ((value) =>
+            setState(() => valueDestinationDomestic = value)),
+      ),
     );
   }
 
@@ -144,7 +183,10 @@ class _FlightsPageState extends State<FlightsPage>
           ),
           body: TabBarView(children: <Widget>[
             //Two-Way
-            Container(color: Colors.amber[300], child: DomesticOrigin()),
+            Container(
+              color: Colors.amber[300],
+              child: SizedBox(child: DomesticOrigin()),
+            ),
             //One-Way
             Container(
               color: Colors.amber[200],
